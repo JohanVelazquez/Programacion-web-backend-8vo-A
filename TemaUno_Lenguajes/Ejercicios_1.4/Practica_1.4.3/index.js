cd86806e400f9fa7412aaa0602ab57c1859462ff
Johan Velazquez <221k0030@itscarrillopuerto.edu.mx>
2026-03-05 22:43:32 -0600

    Agrego actividad 1.4.3 05/03/2026

---
+++ TemaUno_Lenguajes/Ejercicios_1.4/Practica_1.4.3/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+/* import.meta.url proporciona la URL del módulo actual.
+fileURLToPath(import.meta.url) convierte esa URL en una ruta de archivo.
+dirname() recupera el nombre del directorio a partir de la ruta del archivo. */
+
+console.log(__dirname);
+
+const app = express();
+const port = 3000;
+
+// Middleware de body-parser para procesar formularios
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Servir el formulario index.html
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// Ruta POST para recibir datos del formulario
+app.post("/submit", (req, res) => {
+  console.log(req.body); // imprime en consola los datos enviados
+  res.send("Datos recibidos");
+});
+
+// Iniciar servidor
+app.listen(port, () => {
+  console.log(`Servidor ejecutándose en el puerto ${port}`);
+});
---
+++ TemaUno_Lenguajes/Ejercicios_1.4/Practica_1.4.3/index2.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+var nombreEquipo = "";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function registrador(req, res, next) {
+  console.log(req.body);
+  nombreEquipo = req.body["mascota"] + req.body["adjetivo"];
+  next();
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", registrador, (req, res) => {
+  console.log(req.body);
+  res.send("<h1>El nombre de tu equipo es:</h1><h2>" + nombreEquipo + " ✌️</h2>");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor ejecutándose en el puerto ${port}`);
+});
